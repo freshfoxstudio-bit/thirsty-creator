7ad14280284abb925380a9072c70537275840a37
freshfoxstudio-bit <freshfoxstudio@gmail.com>
2026-01-28 17:47:45 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // global styles
+import './index.css'; // optional, include if you have CSS
 
+// Create root and render your app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
